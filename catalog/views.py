52a2b53d14a27edce5299c746bfd1cd573a8e533
Haven Vo <haven@enclave.vn>
2020-07-30 16:55:28 +0700

    Add author list and author detail view

--- catalog/views.py
+++ catalog/views.py
@@ -18,7 +18,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_genres': num_genres
+        'num_genres': num_genres,
     }
 
     return render(request, 'index.html', context=context)
@@ -42,3 +42,22 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Author.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
